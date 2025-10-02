d4413573eae433a81c2a1b7866e0ae9e225aaea0
katekime <katekime07@gmail.com>
2025-10-03 01:14:59 +0500

    maxheap algorithm implementation

--- src/main/java/org/algorithm/MaxHeap.java
+++ src/main/java/org/algorithm/MaxHeap.java
@@ -1,4 +1,73 @@
 package org.algorithm;
 
 public class MaxHeap {
+    private int[] heap;
+    private int size;
+    private int capacity;
+
+    public MaxHeap(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.heap = new int[capacity];
+    }
+
+    public void insert(int value) {
+        if (size == capacity) {
+            throw new IllegalStateException("heap is full");
+        }
+        heap[size] = value;
+        heapifyUp(size);
+        size++;
+    }
+
+    public int extractMax() {
+        if (size == 0) {
+            throw new IllegalStateException("heap is empty");
+        }
+        int max = heap[0];
+        heap[0] = heap[size - 1];
+        size--;
+        heapifyDown(0);
+        return max;
+    }
+
+    private void heapifyUp(int index) {
+        int parent = (index - 1) / 2;
+        while (index > 0 && heap[index] > heap[parent]) {
+            swap(index, parent);
+            index = parent;
+            parent = (index - 1) / 2;
+        }
+    }
+
+    private void heapifyDown(int index) {
+        while (index < size) {
+            int left = index * 2 + 1;
+            int right = index * 2 + 2;
+            int largest = index;
+
+            if (left < size && heap[left] > heap[largest]) {
+                largest = left;
+            }
+            if (right < size && heap[right] > heap[largest]) {
+                largest = right;
+            }
+            if (largest != index) {
+                swap(index, largest);
+                index = largest;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void swap(int i, int j) {
+        int tmp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = tmp;
+    }
+
+    public int getSize() {
+        return size;
+    }
 }
--- src/test/java/algorithm/MaxHeapTest.java
+++ src/test/java/algorithm/MaxHeapTest.java
@@ -1,4 +1,27 @@
 package algorithm;
 
+import org.algorithm.MaxHeap;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class MaxHeapTest {
+    @Test
+    void testInsertAndExtractMax() {
+        MaxHeap heap = new MaxHeap(10);
+        heap.insert(5);
+        heap.insert(20);
+        heap.insert(15);
+
+        assertEquals(20, heap.extractMax());
+        assertEquals(15, heap.extractMax());
+        assertEquals(5, heap.extractMax());
+    }
+
+    @Test
+    void testHeapEmptyExtract() {
+        MaxHeap heap = new MaxHeap(5);
+        assertThrows(IllegalStateException.class, heap::extractMax);
+    }
 }
